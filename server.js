147fa59eeceadcc56e0ba2a58fde0de56854d468
shyboy443 <ashen.semithu@gmail.com>
2024-02-19 22:53:51 +0530

    sd

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:3000","http://shy-app.onrender.com"],
+    origin: ["http://localhost:3000","https://shy-app.onrender.com"],
     credentials: true,
 }));
 app.use("/uploads",express.static(path.join(__dirname,"uploads")));
